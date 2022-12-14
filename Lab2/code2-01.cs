249337d87456bd492703250ff24dfa5d9d77084b
Mo <118350624+chattarin052545@users.noreply.github.com>
2022-12-14 12:37:59 +0700

    Add files via upload

---
+++ Lab2/code2-01.cs
@@ -0,0 +1,13 @@
+using  System;
+
+namespace HelloWorld
+{
+    class Program
+}
+static void Main(string[]args)
+{
+    String a ="good morning";
+    String b = "good evening";
+    Console.WriteLine("Hello Woerld!");
+    Console.WriteLine("{0}{1}",b,a);
+}
---
+++ Lab2/code2-04.js
@@ -0,0 +1,5 @@
+const message = 'Hello world'
+let egn = 'god good';
+let hot = 'no nut';
+document.querySelector('#header').innerHTML = message
+console.log(message,egn,hot)
